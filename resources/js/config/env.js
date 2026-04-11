7b32e7246c7d5d98357438cbb4196df115839d1b
Truong <quangdao@gmail.com>
2026-04-11 20:27:17 +0700

    change name config env

--- resources/js/config/env.js
+++ resources/js/config/env.js
@@ -1,6 +1,6 @@
 const ENV = {
     API_URL: window.APP_CONFIG?.API_URL || process.env.MIX_HOST,
-    API_KEY: window.APP_CONFIG?.API_KEY || process.env.MIX_API_KEY,
+    API_KEY: window.APP_CONFIG?.MIX_API_KEY || process.env.MIX_API_KEY,
     GOOGLE_MAP_KEY: window.APP_CONFIG?.GOOGLE_MAP_KEY || process.env.MIX_GOOGLE_MAP_KEY,
     DEMO: window.APP_CONFIG?.DEMO || process.env.MIX_DEMO,
     PUSHER_APP_KEY: window.APP_CONFIG?.PUSHER_APP_KEY || process.env.MIX_PUSHER_APP_KEY,
